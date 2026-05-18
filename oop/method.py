3440bfd3a142fd54821a334225175d82925aa502
antonyjoby28 <antonyjoby28@gmail.com>
2026-05-18 14:16:33 +0530

    method

---
+++ oop/method.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(self,name):
+        self.name = name
+
+    def gret(self):
+        print("Hello, my name is "+self.name)
+
+p1 = person("johny sins",32)
+p1.gret()
+        
