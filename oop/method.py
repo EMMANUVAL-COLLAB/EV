07376590010c6575a53f54a71951d1c6be4aff5a
antonyjoby28 <antonyjoby28@gmail.com>
2026-05-18 14:33:23 +0530

    inhritance

--- oop/method.py
+++ oop/method.py
@@ -1,10 +1,18 @@
 class person:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self, fname,lname):
+        self.fname = fname
+        self.lname  = lname
 
-    def gret(self):
-        print("Hello, my name is "+self.name)
+    def printname(self):
+        print(self.fname, self.lname)
+    
 
-p1 = person("johny sins",32)
-p1.gret()
-        
+p1 = person("johny", "Mcov")
+p1.printname()
+
+class student(person):
+    pass
+
+s = student("Hmaxa","Sutha")
+s.printname()
+
