9815d35a16b9a15fee06cf0a0f115ccef5a1bdb2
antonyjoby28 <antonyjoby28@gmail.com>
2026-05-18 12:37:03 +0530

    oop class object creation

--- oop/object.py
+++ oop/object.py
@@ -1,19 +1,29 @@
-class Tv:
-    def On(self):
-        print("Tv on....")
+# class Tv:
+#     def On(self):
+#         print("Tv on....")
 
-class Fan:
-    def On ( self):
-        print("Fan on....")
+# class Fan:
+#     def On ( self):
+#         print("Fan on....")
 
-class Mixi:
-    def On (self):
-        print("Mixi on....")
+# class Mixi:
+#     def On (self):
+#         print("Mixi on....")
 
-tv=Tv()
-fan=Fan()
-mixi=Mixi() 
+# tv=Tv()
+# fan=Fan()
+# mixi=Mixi() 
     
-tv.On()
-fan.On()
-mixi.On()
+# tv.On()
+# fan.On()
+# mixi.On()
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Emil",12)
+
+print(p1.name)
+print(p1.age)
